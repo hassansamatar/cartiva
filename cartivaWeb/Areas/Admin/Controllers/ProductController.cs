2a5f27f4f80ddedafd99563f8b6b9b97fc681fed
Hassan Samatar <hassansamatardeveloper@gmail.com>
2026-03-11 15:35:33 +0100

    Add Upload and update images

--- cartivaWeb/Areas/Admin/Controllers/ProductController.cs
+++ cartivaWeb/Areas/Admin/Controllers/ProductController.cs
@@ -64,6 +64,16 @@ namespace CartivaWeb.Areas.Admin.Controllers
                     string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, @"images\products");
 
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
                     {
                         file.CopyTo(fileStream);
