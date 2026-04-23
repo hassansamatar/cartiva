d5699b9335c0e40d0ddbeea9228aceef84515a17
Hassan Samatar <hassansamatardeveloper@gmail.com>
2026-04-23 04:10:04 +0200

    Fix return request navigation and dropdown binding
    - Fixed 404 issue caused by missing /Customer/Order route
    - Corrected redirects to use Order/History action
    - Migrated return reasons to ReturnVm instead of ViewBag
    - Improved strongly-typed Razor view for return request

---
+++ src/Cartiva.Domain/ViewModels/ReturnVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cartiva.Domain.ViewModels
+{
+    public class ReturnVm
+    {
+        public int OrderDetailId { get; set; }
+
+        public OrderDetail? OrderDetail { get; set; }
+
+        public int DaysRemaining { get; set; }
+
+        public IEnumerable<SelectListItem> Reasons { get; set; } = new List<SelectListItem>();
+    }
+}
